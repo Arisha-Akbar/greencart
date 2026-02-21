575bc19943555d0cf801305ea74da52ca929c696
Arisha-Akbar <arishaakbar713@gmail.com>
2026-02-21 11:12:49 +0500

    Added Bottom Banner, NewsLetter and Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
-
+import Footer from "./components/Footer";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("/seller");
@@ -11,7 +11,7 @@ const App = () => {
     <div>
       {isSellerPath ? null : <Navbar />}
 
-<Toaster/>
+      <Toaster />
 
       <div
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
@@ -20,6 +20,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 };

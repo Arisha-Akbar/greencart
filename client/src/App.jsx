7ca346f74a4dec4cc4b2f60c1478375bab07bbf7
Arisha-Akbar <arishaakbar713@gmail.com>
2026-02-21 13:48:53 +0500

    Added product page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,18 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("/seller");
+  const { showUserLogin } = useAppContext();
+
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
 
       <Toaster />
 
@@ -18,6 +24,7 @@ const App = () => {
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}

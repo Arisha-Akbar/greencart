1331ac41f3b68ef8347870bee38d181994084312
Arisha-Akbar <arishaakbar713@gmail.com>
2026-02-21 13:55:01 +0500

    added search functionality in product page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
-  const { user, setUser, setShowUserLogin, navigate } = useAppContext();
+  const {
+    user,
+    setUser,
+    setShowUserLogin,
+    navigate,
+    setSearchQuery,
+    searchQuery,
+  } = useAppContext();
 
   const logout = async () => {
     setUser(null);
     navigate("/");
   };
 
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      navigate("/products");
+    }
+  }, [searchQuery]);
+
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
-      <NavLink to="/" onClick={()=>{setOpen(false)}}>
+      <NavLink
+        to="/"
+        onClick={() => {
+          setOpen(false);
+        }}
+      >
         <img className="h-9" src={assets.logo} alt="Logo" />
       </NavLink>
 
@@ -26,6 +44,7 @@ const Navbar = () => {
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
+            onChange={(e) => setSearchQuery(e.target.value)}
             className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
             type="text"
             placeholder="Search products"
@@ -33,7 +52,12 @@ const Navbar = () => {
           <img src={assets.search_icon} alt="search" className="" />
         </div>
 
-        <div onClick={()=>{navigate('/cart')}} className="relative cursor-pointer">
+        <div
+          onClick={() => {
+            navigate("/cart");
+          }}
+          className="relative cursor-pointer"
+        >
           <img
             src={assets.nav_cart_icon}
             alt="cart"
@@ -55,10 +79,18 @@ const Navbar = () => {
           <div className="relative group">
             <img src={assets.profile_icon} className="w-10" />
             <ul className="hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40">
-              <li onClick={()=>{navigate('/my-orders')}} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">
+              <li
+                onClick={() => {
+                  navigate("/my-orders");
+                }}
+                className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer"
+              >
                 My Orders
               </li>
-              <li onClick={logout} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">
+              <li
+                onClick={logout}
+                className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer"
+              >
                 Logout
               </li>
             </ul>

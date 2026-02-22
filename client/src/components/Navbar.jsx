0f49a08538921e110f3cc26482a35cf5054ba5b7
Arisha-Akbar <arishaakbar713@gmail.com>
2026-02-22 23:49:43 +0500

    improved cart icon placement and menu button functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -65,7 +65,6 @@ const Navbar = () => {
             className="w-6 opacity-80"
           />
           <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-4.5 h-4.5 rounded-full">
-            
             {getCartCount()}
           </button>
         </div>
@@ -99,15 +98,31 @@ const Navbar = () => {
           </div>
         )}
       </div>
+      <div className="flex items-center gap-6 sm:hidden">
+        <div
+          onClick={() => {
+            navigate("/cart");
+          }}
+          className="relative cursor-pointer"
+        >
+          <img
+            src={assets.nav_cart_icon}
+            alt="cart"
+            className="w-6 opacity-80"
+          />
+          <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-4.5 h-4.5 rounded-full">
+            {getCartCount()}
+          </button>
+        </div>
 
-      <button
-        onClick={() => (open ? setOpen(false) : setOpen(true))}
-        aria-label="Menu"
-        className="sm:hidden"
-      >
-        {/* Menu Icon SVG */}
-        <img src={assets.menu_icon} alt="menu" />
-      </button>
+        <button
+          onClick={() => (open ? setOpen(false) : setOpen(true))}
+          aria-label="Menu"
+        >
+          {/* Menu Icon SVG */}
+          <img src={assets.menu_icon} alt="menu" />
+        </button>
+      </div>
 
       {/* Mobile Menu */}
       {open && (

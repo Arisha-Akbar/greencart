4efd1da2967d8975c65389d08631ea7e321165fa
Arisha-Akbar <arishaakbar713@gmail.com>
2026-04-14 20:26:15 +0500

    fix: adjust layout spacing in BestSeller and ProductCard components

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -15,7 +15,7 @@ const ProductCard = ({ product }) => {
           );
           window.scrollTo({ top: 0, behavior: "smooth" })
         }}
-        className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full max-w-xs mx-auto"
+        className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full mx-auto"
       >
         <div className="group cursor-pointer flex items-center justify-center px-2">
           <img
